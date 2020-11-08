9ae0c6b4e253500aff15ce24481e41cb2e9a4f78
Madhurima Mishra <mail2sikun@gmail.com>
2020-11-08 20:18:03 +0530

    all file has been done

---
+++ jsclock/app.js
@@ -0,0 +1,22 @@
+const hourHand= document.querySelector(".hour-hand");
+const minuteHand= document.querySelector(".minute-hand");
+const secHand= document.querySelector(".sec-hand");
+
+function setDate(){
+const now = new Date();
+
+const second =now.getSeconds();
+const secondDegrees = ((second/ 60)* 360) + 90;
+secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+const minute= now.getMinutes();
+const minuteDegrees= ((minute/60) *360)+ 90;
+minuteHand.style.transform =`rotate(${minuteDegrees}deg)`;
+
+const hour= now.getHours();
+const hourDegrees= ((hour/12) *360)+ 90;
+hourHand.style.transform =`rotate(${hourDegrees}deg)`;
+
+}
+setInterval(setDate,1000);
+setDate();
