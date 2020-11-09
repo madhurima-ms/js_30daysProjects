1232a783488d9d474f9d5c3c54a05a37fe6e33f3
Madhurima Mishra <mail2sikun@gmail.com>
2020-11-09 10:31:26 +0530

    minHand has been updated

--- jsclock/app.js
+++ jsclock/app.js
@@ -13,7 +13,7 @@ secHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
 const mins = now.getMinutes();
 const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 
 const hour = now.getHours();
 const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
