dc924728121e7598fe0314a7d795cb234bfafcad
Madhurima Mishra <mail2sikun@gmail.com>
2020-11-18 23:42:12 +0530

    js file added

--- updateCSSHTML/app.js
+++ updateCSSHTML/app.js
@@ -1,10 +1,10 @@
-const inputs = document.querySelectorAll(".controls input");
 
-function handleUpdate(){
-    const suffix= this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-}
+    const inputs = document.querySelectorAll('.controls input');
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+    function handleUpdate() {
+      const suffix = this.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
 
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
