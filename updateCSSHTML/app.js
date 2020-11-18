bfd2a2402397ffe59641bcc7c7b17a8b82f9506d
Madhurima Mishra <mail2sikun@gmail.com>
2020-11-18 23:38:03 +0530

    js file added

--- updateCSSHTML/app.js
+++ updateCSSHTML/app.js
@@ -2,5 +2,9 @@ const inputs = document.querySelectorAll(".control inputs");
 
 function handleUpdate(){
     const suffix= this.dataset.sizing || '';
-    
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
