6ebbd9b5339247b7f67cc792f72be086b47cb64e
Madhurima Mishra <mail2sikun@gmail.com>
2020-11-21 15:40:42 +0530

    without js file

--- updateCSSHTML/app.js
+++ updateCSSHTML/app.js
@@ -4,9 +4,11 @@
     function handleUpdate() {
     const suffix = this.dataset.sizing || '';
     console.log(suffix);
+    console.log(this);
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
     }
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('', handleUpdate));
-    console.log(handleUpdate());
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  
