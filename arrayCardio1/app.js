78d7d8fd2bda5a67a25b7123f772538069839c2c
Madhurima Mishra <mail2sikun@gmail.com>
2020-12-17 19:06:42 +0530

    all the methods has been implemented

--- arrayCardio1/app.js
+++ arrayCardio1/app.js
@@ -23,6 +23,7 @@ const inventors = [
   
   // Array.prototype.filter()
   // 1. Filter the list of inventors for those who were born in the 1500's
+  
 const fifteen= inventors.filter(function(inventor){
 if(inventor.year>=1500 && inventor.year<1600){
     return true;
