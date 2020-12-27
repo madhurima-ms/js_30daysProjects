b9a69f75a3b3a24619f3f84073eac486722f9170
Madhurima Mishra <mail2sikun@gmail.com>
2020-12-27 23:30:17 +0530

    css file removed

--- htmlCanvas/app.js
+++ htmlCanvas/app.js
@@ -1,5 +1,6 @@
-const canvas = document.querySelector("#draw");
-const ctx= canvas.getContext('2d');
+
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 ctx.strokeStyle = '#BADA55';
