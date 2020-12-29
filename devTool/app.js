d8765e703e5c3d71f20fec2e13f930d857325512
Madhurima Mishra <mail2sikun@gmail.com>
2020-12-29 16:35:00 +0530

    all the methods has been implemented

--- devTool/app.js
+++ devTool/app.js
@@ -30,46 +30,45 @@ console.dir(p);
 // console.clear();
 
 // //Grouping together 
-dogs.forEach(dog=>{
-    console.group(`${dog.name}`);
-    console.log(`This is ${dog.age} years old`);
-    console.log(`${dog.name} is ${dog.age} years old`);
-});
-
-
-
-
 // dogs.forEach(dog=>{
 //     console.groupCollapsed(`${dog.name}`);
-//       console.log(`This is ${dog.name}`);
-//       console.log(`${dog.name} is ${dog.age} years old`);
-//       console.log(`${dog.name} is ${dog.age * 7} dog years old`);
-//       console.groupEnd(`${dog.name}`);
-//     });
+//     console.log(`This is ${dog.name}`);
+//     console.log(`${dog.name} is ${dog.age} years old`);
+//     console.groupEnd(`${dog.name}`);
+// });
 
-// //     // counting
-//     console.count('Wes');
-//     console.count('Wes');
-//     console.count('Steve');
-//     console.count('Steve');
-//     console.count('Wes');
-//     console.count('Steve');
-//     console.count('Wes');
-//     console.count('Steve');
-//     console.count('Steve');
-//     console.count('Steve');
-//     console.count('Steve');
-//     console.count('Steve');
 
-//     // timing
-//     console.time('fetching data');
-//     fetch('https://api.github.com/users/wesbos')
-//       .then(data => data.json())
-//       .then(data => {
-//         console.timeEnd('fetching data');
-//        console.log(data);
-//       });
 
-//     console.table(dogs);
-// console.time('fetching data');
-// fetch('')
+
+dogs.forEach(dog=>{
+    console.groupCollapsed(`${dog.name}`);
+      console.log(`This is ${dog.name}`);
+      console.log(`${dog.name} is ${dog.age} years old`);
+      console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+      console.groupEnd(`${dog.name}`);
+    });
+
+//     // counting
+    console.count('Wes');
+    console.count('Wes');
+    console.count('Steve');
+    console.count('Steve');
+    console.count('Wes');
+    console.count('Steve');
+    console.count('Wes');
+    console.count('Steve');
+    console.count('Steve');
+    console.count('Steve');
+    console.count('Steve');
+    console.count('Steve');
+
+    // timing
+    console.time('fetching data');
+    fetch('https://api.github.com/users/wesbos')
+      .then(data => data.json())
+      .then(data => {
+        console.timeEnd('fetching data');
+       console.log(data);
+      });
+
+//     
