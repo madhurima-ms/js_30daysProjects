6b765399eb000f80f08457cdbeb048fbc94f067e
Madhurima Mishra <mail2sikun@gmail.com>
2020-12-29 12:13:41 +0530

    all the methods has been implemented

--- devTool/app.js
+++ devTool/app.js
@@ -3,53 +3,73 @@ const dogs = [{ name: 'Snickers', age: 2 },
 
 function makeGreen(){
     const p= document.querySelector('p');
-    p.style.color="#BADA55";
-    p.style.font="10px";
+    p.style.color="black";
+    p.style.font="30px";
+    p.style.fontFamily="Times New Roman", Times, serif;
 }
 console.log('hello');
 console.log('Hello I am a %s string!', '💩');
+//Warning
 console.warn('OH NO!');
+
+//Error
 console.error('oh shit!');
+
+//Info
 console.info('Crocodiles eat 3-4 people per year');
 
-const p = document.querySelector('p');
-console.assert(p.classList.contains('.ouch'), 'That is wrong');
-//Clearing
+//Testing
+ const p = document.querySelector('p');
+ console.assert(p.classList.contains('.ouch'), 'That is wrong');
+
+// //Clearing
 console.clear();
+
 console.log(p);
 console.dir(p);
-console.clear();
+// console.clear();
 
-//Grouping together 
+// //Grouping together 
 dogs.forEach(dog=>{
-    console.groupCollapsed(`${dog.name}`);
-      console.log(`This is ${dog.name}`);
-      console.log(`${dog.name} is ${dog.age} years old`);
-      console.log(`${dog.name} is ${dog.age * 7} dog years old`);
-      console.groupEnd(`${dog.name}`);
-    });
+    console.group(`${dog.name}`);
+    console.log(`This is ${dog.age} years old`);
+    console.log(`${dog.name} is ${dog.age} years old`);
+});
 
-    // counting
-    console.count('Wes');
-    console.count('Wes');
-    console.count('Steve');
-    console.count('Steve');
-    console.count('Wes');
-    console.count('Steve');
-    console.count('Wes');
-    console.count('Steve');
-    console.count('Steve');
-    console.count('Steve');
-    console.count('Steve');
-    console.count('Steve');
 
-    // timing
-    console.time('fetching data');
-    fetch('https://api.github.com/users/wesbos')
-      .then(data => data.json())
-      .then(data => {
-        console.timeEnd('fetching data');
-        console.log(data);
-      });
 
-    console.table(dogs);
+
+// dogs.forEach(dog=>{
+//     console.groupCollapsed(`${dog.name}`);
+//       console.log(`This is ${dog.name}`);
+//       console.log(`${dog.name} is ${dog.age} years old`);
+//       console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+//       console.groupEnd(`${dog.name}`);
+//     });
+
+// //     // counting
+//     console.count('Wes');
+//     console.count('Wes');
+//     console.count('Steve');
+//     console.count('Steve');
+//     console.count('Wes');
+//     console.count('Steve');
+//     console.count('Wes');
+//     console.count('Steve');
+//     console.count('Steve');
+//     console.count('Steve');
+//     console.count('Steve');
+//     console.count('Steve');
+
+//     // timing
+//     console.time('fetching data');
+//     fetch('https://api.github.com/users/wesbos')
+//       .then(data => data.json())
+//       .then(data => {
+//         console.timeEnd('fetching data');
+//        console.log(data);
+//       });
+
+//     console.table(dogs);
+// console.time('fetching data');
+// fetch('')
