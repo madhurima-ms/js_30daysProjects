685a07df200d0c4faf4bb88586cc3b4e428cf59e
Madhurima Mishra <mail2sikun@gmail.com>
2021-02-27 20:50:35 +0530

    json file added

--- sortWithoutArticle/app.js
+++ sortWithoutArticle/app.js
@@ -1,17 +1,56 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function strip(bandName) {
-    return bandName.replace(/^(a |the |an )/i, '').trim();
-  }
-  const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-document.querySelector('#bands').innerHTML = sortedBands .map(band => `<li>${band}</li>`) .join('');
 
+
+function ignoreLeadingArticle(bandName){
+        return bandName.replace(/^(a |the |an )/i, '').trim();
+     }
+
+
+
+   
+const sortedBands= bands.sort((a,b)=>{
+    return ignoreLeadingArticle(a)> ignoreLeadingArticle(b) ? 1: -1;
+  
+});
 console.log(sortedBands);
 
-// function article(bandName){
-//     return bandName.replace(/^(a |the |an )/i, '').trim();
 
+
+document.querySelector('#bands').innerHTML= bands.map(band=>`<li>${band}</li>`).join('');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function ignoreLeadingArticle(bandName){
+//     return bandName.replace(/^(a |the |an )/i, '').trim();
 // }
-// const sortedBand = bands.sort((a,b)=>article(a) > article(b) ?1 : -1);
-// document.querySelector('#bands').innerHTML= sortedBand.map(band=>`<li>${band}</li>`).join('');
+// const sortedBands= bands.sort((a,b)=>ignoreLeadingArticle(a)>ignoreLeadingArticle(b)? 1: -1);
+// document.querySelector('#bands').innerHTML= sortedBands.map(band=>`<li>${band}</li>`).join('');
+// console.log(sortedBands);
+
+
+
+
+
+
