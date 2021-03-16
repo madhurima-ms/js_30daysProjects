e834f0a4c795a684d924e7881f93a057cde4d0e3
Madhurima Mishra <mail2sikun@gmail.com>
2021-03-16 16:36:07 +0530

    find the length of the hole

---
+++ whackMoleGame/app.js
@@ -0,0 +1,10 @@
+const mole= document.querySelectorAll('.moles');
+const hole= document.querySelectorAll('.holes');
+const searchBoard= document.querySelector('.score');
+
+function randomTime(min,max){
+    return Math.round(Math.random * (max-min) *min );
+}
+ function randomHole(holes){
+     console.log(hole.length);
+ }
